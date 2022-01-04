5226c6b521f94ce9e7eeb40a8b57205b9b13b63b
Niall Egan <negan1998@gmail.com>
2022-01-04 14:50:23 +0100

    Pin Python client version

--- cmdexec/clients/python/setup.py
+++ cmdexec/clients/python/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="databricks-sql-connector",
-    version="0.0.0",
+    version="2.0.0-rc1",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     install_requires=["pyarrow", 'thrift>=0.10.0', "pandas"],
