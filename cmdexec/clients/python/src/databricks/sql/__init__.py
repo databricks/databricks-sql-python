f118da54122177c5efa89361e1eedfec65edcfc2
Niall Egan <negan1998@gmail.com>
2022-03-07 10:35:52 +0100

    Fix python packaging problem
    ## What changes are proposed in this pull request?
       See http://go/pr-description for tips on what to write here
       New to Databricks? See http://go/pr-process
    
       [NEW] Please review if shiproom approval is required.
       See the Shiproom section below for details.
    Looking at https://setuptools.pypa.io/en/latest/userguide/declarative_config.html?highlight=setup.cfg, we did not have the setup.cfg quite right so dependencies were not being installed properly.
    
    ## How is this tested?
      - Added unit or integration tests? Please mention them here.
      - Performed manual testing? Please describe your testing procedure.
      - Was testing unnecessary or not possible? Please explain why.
    Verified that `pip install dist/databricks_sql_connector-2.0.0b0-py3-none-any.whl` also installed dependencies
    
    ## How is this feature monitored?
      - Did you add usage logs or metrics? Please mention them here.
      - Create dashboards or monitoring notebooks? Please link them here.
      - See http://go/obs/user for docs on our observability tools.
    
    #### Approvals
    
    #### [NEW] Shiproom
    
      - [ ] A "material change" covering this PR exists in http://go/engshiproom: `<CHANGE_ID>`
    
      - [ ] Change is **NOT** a “material change”
      - [ ] Change **IS** a “material change” of low / medium risk
      - [ ] Change **IS** a “material change” of high risk needing Shiproom review
    
    #### Security implications
    #153380 is resolved by databricks/master.

--- cmdexec/clients/python/src/databricks/sql/__init__.py
+++ cmdexec/clients/python/src/databricks/sql/__init__.py
@@ -27,7 +27,7 @@ DATETIME = DBAPITypeObject('timestamp')
 DATE = DBAPITypeObject('date')
 ROWID = DBAPITypeObject()
 
-__version__ = "2.0.0b0"
+__version__ = "2.0.0b1"
 USER_AGENT_NAME = "PyDatabricksSqlConnector"
 
 # These two functions are pyhive legacy
