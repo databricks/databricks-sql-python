8d32245b04f59af930006664a7221121f194099a
Niall Egan <negan1998@gmail.com>
2022-04-05 15:49:26 +0200

    PySQL release

--- cmdexec/clients/python/src/databricks/sql/__init__.py
+++ cmdexec/clients/python/src/databricks/sql/__init__.py
@@ -27,7 +27,7 @@ DATETIME = DBAPITypeObject('timestamp')
 DATE = DBAPITypeObject('date')
 ROWID = DBAPITypeObject()
 
-__version__ = "2.0.0b1"
+__version__ = "2.0.0b2"
 USER_AGENT_NAME = "PyDatabricksSqlConnector"
 
 # These two functions are pyhive legacy
