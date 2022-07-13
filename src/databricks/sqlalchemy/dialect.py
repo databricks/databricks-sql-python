e13fb3b0e2d279713f5d36a5881872ef8784f155
George Chow <george@overcoil.com>
2022-07-12 17:18:48 -0700

    barebone (& non-working) DatabricksDialect implementation
    -a bare-bone dialect styled after PyHive and Impyla
    -includes minimal/stubs for DatabricksIdentifierPreparer, DatabricksExecutionContext, DatabricksTypeCompiler, & DatabricksCompiler

--- src/databricks/sqlalchemy/dialect.py
+++ src/databricks/sqlalchemy/dialect.py
@@ -1,25 +1,133 @@
 from databricks import sql
-from typing import AnyStr
+from databricks import sql as dbsql
+
+# cribbing from Hive
+from pyhive.sqlalchemy_hive import HiveExecutionContext, HiveIdentifierPreparer, HiveCompiler, HiveTypeCompiler
+from pyhive.sqlalchemy_hive import _type_map
+
+import re
 
 from sqlalchemy import types
 from sqlalchemy import util
 
-from sqlalchemy.engine import default
+from sqlalchemy.engine import default, interfaces
+from sqlalchemy.sql import compiler
+
+from typing import AnyStr
+
+
+class DatabricksIdentifierPreparer(compiler.IdentifierPreparer):
+    # SparkSQL identifier specification:
+    # ref: https://spark.apache.org/docs/latest/sql-ref-identifier.html
+
+    legal_characters = re.compile(r'^[A-Z0-9_]+$', re.I)
+
+    def __init__(self, dialect):
+        super(DatabricksIdentifierPreparer, self).__init__(
+            dialect,
+            initial_quote='`',
+        )
+
+
+class DatabricksExecutionContext(default.DefaultExecutionContext):
+    # There doesn't seem to be any override of DefaultExecutionContext required
+    # but I will nonetheless introduce this class for clarity
+
+    # TODO: revisit server-side cursors
+    # ref: https://docs.databricks.com/dev-tools/python-sql-connector.html#manage-cursors-and-connections
+    pass
+
+
+class DatabricksTypeCompiler(compiler.GenericTypeCompiler):
+    # ref: https://spark.apache.org/docs/latest/sql-ref-datatypes.html
+
+    def visit_TINYINT(self, type_):
+        return 'TINYINT'
+
+    def visit_SMALLINT(self, type_, **kw):
+        return "SMALLINT"
+
+    def visit_INTEGER(self, type_, **kw):
+        return "INT"
+
+    def visit_BIGINT(self, type_, **kw):
+        return "BIGINT"
+
+
+    def visit_FLOAT(self, type_, **kw):
+        return "FLOAT"
+
+    def visit_DOUBLE(self, type_, **kw):
+        return "DOUBLE"
+
+
+    def visit_DECIMAL(self, type_, **kw):
+        if type_.precision is None:
+            return "DECIMAL"
+        elif type_.scale is None:
+            return "DECIMAL(%(precision)s)" % {"precision": type_.precision}
+        else:
+            return "DECIMAL(%(precision)s, %(scale)s)" % {
+                "precision": type_.precision,
+                "scale": type_.scale,
+            }
+
+
+    def visit_DATE(self, type_, **kw):
+        return "DATE"
+
+    def visit_TIMESTAMP(self, type_, **kw):
+        return "TIMESTAMP"
+
+
+    def visit_BOOLEAN(self, type_, **kw):
+        return "BOOLEAN"
+
+    def visit_STRING(self, type_, **kw):
+        return "STRING"
+
+
+class DatabricksCompiler(compiler.SQLCompiler):
+    # stub
+    pass
+
 
 
 class DatabricksDialect(default.DefaultDialect):
-
     # Possible attributes are defined here: https://docs.sqlalchemy.org/en/14/core/internals.html#sqlalchemy.engine.Dialect
     name: str = "databricks"
     driver: str= "thrift"
     default_schema_name: str = "default"
 
+    preparer = DatabricksIdentifierPreparer
+    execution_ctx_cls = DatabricksExecutionContext
+    statement_compiler = DatabricksCompiler
+    type_compiler = DatabricksTypeCompiler
+
+    # the following attributes are cribbed from HiveDialect:
+    supports_views = False
+    supports_alter = True
+    supports_pk_autoincrement = False
+    supports_default_values = False
+    supports_empty_insert = False
+    supports_native_decimal = True
+    supports_native_boolean = True
+    supports_unicode_statements = True
+    supports_unicode_binds = True
+    returns_unicode_strings = True
+    description_encoding = None
+    supports_multivalues_insert = True
+    supports_sane_rowcount = False
+
+
     @classmethod
     def dbapi(cls):
         return sql
 
-    def create_connect_args(self, url):
+    def create_connect_args(self, url: "URL"):
         # Expected URI format is: databricks+thrift://token:dapi***@***.cloud.databricks.com?http_path=/sql/***
+
+        # TODO: add schema in
 
         kwargs = {
             "server_hostname": url.host,
@@ -29,17 +137,83 @@ class DatabricksDialect(default.DefaultDialect):
 
         return [], kwargs
 
-    def get_table_names(self, *args, **kwargs):
+    # def initialize(self, connection) -> None:
+    #     super().initialize(connection)
 
-        # TODO: Implement with native driver `.tables()` call
-        return super().get_table_names(*args, **kwargs)
+    def get_schema_names(self, connection, **kw):
+        connection = dbsql.connect(
+            server_hostname=kwargs['server_hostname'],
+            http_path=kwargs['http_path'],
+            access_token=kwargs['access_token'],
+            schema='default'
+        )
+        TABLE_SCHEM = 2
+        with connection.cursor() as cur:
+            data = cur.schemas(catalog_name='%').fetchall()
+            _schemas = [i[TABLE_SCHEM] for i in data]
 
-    def get_columns(self, *args, **kwargs):
+        return _schemas
 
+    def get_table_names(self, connection, schema = None, **kw):
+        # TODO: can initialize() take care of shared initialization?
+        connection = dbsql.connect(
+            server_hostname=kwargs['server_hostname'],
+            http_path=kwargs['http_path'],
+            access_token=kwargs['access_token'],
+            schema='default'
+        )
+
+        breakpoint()
+
+        TABLE_NAME = 2
+        with connection.cursor() as cur:
+            data = cur.tables(schema_name='default').fetchall()
+            _tables = [i[TABLE_NAME] for i in data]
+
+        return _tables
+
+
+    def get_columns(self, connection, table_name, schema=None, **kw):
         # TODO: Implement with native driver `.columns()` call
+        return self._get_table_columns(connection, table_name, schema)
 
-        return super().get_columns(*args, **kwargs)
+    
+    def get_view_names(self, connection, schema=None, **kw):
+        # no views at present
+        return []
 
-    def do_rollback(self, dbapi_connection):
-        # Databricks SQL Does not support transaction
+    # private method to serve get_columns() and has_tables()
+    def _get_table_columns(self, connection, table_name, schema):
+        with connection.cursor() as cur:
+            data = cur.columns(schema_name='default', table_name=table_name).fetchall()
+            _tables = [i[COLUMN_NAME] for i in data]
+        return _tables
+
+    def has_table(
+        self,
+        connection,
+        table_name,
+        schema = None,
+        **kw,
+    ) -> bool:
+        try:
+            self._get_table_columns(connection, table_name, schema)
+            return True
+        except exc.NoSuchTableError:
+            return False
+
+    def has_table(self, connection, table_name, schema=None):
+        # Spark has no foreign keys
+        return []
+
+    def has_table(self, connection, table_name, schema=None):
+        # Spark has no primary keys
+        return []
+
+    def get_indexes(self, connection, table_name, schema=None, **kw):
+        # TODO: treat partitions as indices
+        return []
+
+    def do_rollback(self, dbapi_connection) -> None:
+        # Spark/Delta transaction only covers single-table updates... to simplify things, just skip this for now.
         pass
--- tests/sqlalchemy/test_suite.py
+++ tests/sqlalchemy/test_suite.py
@@ -1,2 +1,53 @@
 from sqlalchemy.testing.suite import *
 
+from sqlalchemy.testing.suite import (
+    CastTypeDecoratorTest as _CastTypeDecoratorTest,
+)
+from sqlalchemy.testing.suite import (
+    ComponentReflectionTest as _ComponentReflectionTest,
+)
+from sqlalchemy.testing.suite import (
+    ComponentReflectionTestExtra as _ComponentReflectionTestExtra,
+)
+from sqlalchemy.testing.suite import DateTimeTest as _DateTimeTest
+from sqlalchemy.testing.suite import (
+    DifficultParametersTest as _DifficultParametersTest,
+)
+from sqlalchemy.testing.suite import ExistsTest as _ExistsTest
+from sqlalchemy.testing.suite import (
+    ExpandingBoundInTest as _ExpandingBoundInTest,
+)
+from sqlalchemy.testing.suite import (
+    FetchLimitOffsetTest as _FetchLimitOffsetTest,
+)
+from sqlalchemy.testing.suite import InsertBehaviorTest as _InsertBehaviorTest
+from sqlalchemy.testing.suite import IntegerTest as _IntegerTest
+from sqlalchemy.testing.suite import JoinTest as _JoinTest
+from sqlalchemy.testing.suite import LikeFunctionsTest as _LikeFunctionsTest
+from sqlalchemy.testing.suite import (
+    LongNameBlowoutTest as _LongNameBlowoutTest,
+)
+from sqlalchemy.testing.suite import NumericTest as _NumericTest
+from sqlalchemy.testing.suite import OrderByLabelTest as _OrderByLabelTest
+from sqlalchemy.testing.suite import (
+    QuotedNameArgumentTest as _QuotedNameArgumentTest,
+)
+from sqlalchemy.testing.suite import TableDDLTest as _TableDDLTest
+
+
+
+class JoinTest():
+    def test_inner_join_true(self):
+        breakpoint()
+        return
+
+    @testing.skip("databricks")
+    def test_inner_join_false(self):
+        return
+
+    @testing.skip("databricks")
+    def test_outer_join_false(self):
+        return
+
+
+
