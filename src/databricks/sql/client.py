472209c6f886cab1e9ff15cfcae6413a7da89822
Jacky Hu <jacky.hu@databricks.com>
2022-12-29 12:23:31 -0800

    Change oauth redirect port range to port
    Signed-off-by: Jacky Hu <jacky.hu@databricks.com>

--- src/databricks/sql/client.py
+++ src/databricks/sql/client.py
@@ -61,10 +61,11 @@ class Connection:
                 Note this is beta (private preview)
 
             oauth_client_id: `str`, optional
-                custom oauth client_id. If not specified, it will use the client_id of databricks-sql-python
+                custom oauth client_id. If not specified, it will use the built-in client_id of databricks-sql-python.
 
-            oauth_redirect_port_range: `List[str]`, optional
-                oauth redirect port range. This is required when custom oauth client_id `oauth_client_id` is set
+            oauth_redirect_port: `int`, optional
+                port of the oauth redirect uri (localhost). This is required when custom oauth client_id
+                `oauth_client_id` is set
 
             experimental_oauth_persistence: configures preferred storage for persisting oauth tokens.
                 This has to be a class implementing `OAuthPersistence`.
