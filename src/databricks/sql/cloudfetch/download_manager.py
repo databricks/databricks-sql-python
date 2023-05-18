3c3ca0130e5baf77e63267d1d80e8165b0ab3e97
Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
2023-05-17 17:21:20 -0700

    Creating DownloadableResultSettings named tuple and populate with one setting
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>

--- src/databricks/sql/cloudfetch/download_manager.py
+++ src/databricks/sql/cloudfetch/download_manager.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from concurrent.futures import ThreadPoolExecutor
 from databricks.sql.cloudfetch.downloader import ResultSetDownloadHandler
 
@@ -5,15 +6,28 @@ from databricks.sql.cloudfetch.downloader import ResultSetDownloadHandler
 class ResultFileDownloadManager:
     _instance = None
 
-    def __init__(self):
+    def __init__(self, connection):
         self.download_handlers = []
+        self.downloadable_result_settings = _get_downloadable_result_settings(connection)
 
-    def __new__(cls):
+    def __new__(cls, connection):
         if not cls._instance:
-            cls._instance = super(ResultFileDownloadManager, cls).__new__(cls)
+            cls._instance = super(ResultFileDownloadManager, cls).__new__(cls, connection)
 
     def add_file_links(self, t_spark_arrow_result_links):
         for t_spark_arrow_result_link in t_spark_arrow_result_links:
             if t_spark_arrow_result_link.row_count <= 0:
                 continue
             self.download_handlers.append(ResultSetDownloadHandler(None, t_spark_arrow_result_link))
+
+
+DownloadableResultSettings = namedtuple(
+    "DownloadableResultSettings",
+    "is_lz4_compressed"
+)
+
+
+def _get_downloadable_result_settings(connection):
+    return DownloadableResultSettings(
+        is_lz4_compressed=connection.lz4_compression
+    )
