05aca422bdb85f4a65001823657525ac55614c3d
Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
2023-05-17 17:29:40 -0700

    Skeleton for the other downloadable result settings
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>

--- src/databricks/sql/cloudfetch/download_manager.py
+++ src/databricks/sql/cloudfetch/download_manager.py
@@ -18,16 +18,27 @@ class ResultFileDownloadManager:
         for t_spark_arrow_result_link in t_spark_arrow_result_links:
             if t_spark_arrow_result_link.row_count <= 0:
                 continue
-            self.download_handlers.append(ResultSetDownloadHandler(None, t_spark_arrow_result_link))
+            self.download_handlers.append(ResultSetDownloadHandler(
+                self.downloadable_result_settings, t_spark_arrow_result_link))
 
 
 DownloadableResultSettings = namedtuple(
     "DownloadableResultSettings",
-    "is_lz4_compressed"
+    "is_lz4_compressed result_file_link_expiry_buffer download_timeout use_proxy disable_proxy_for_cloud_fetch "
+    "proxy_host proxy_port proxy_uid proxy_pwd"
 )
 
 
 def _get_downloadable_result_settings(connection):
+    # TODO: pipe and get the default values from JDBC
     return DownloadableResultSettings(
-        is_lz4_compressed=connection.lz4_compression
+        is_lz4_compressed=connection.lz4_compression,
+        result_file_link_expiry_buffer=None,
+        download_timeout=0,
+        use_proxy=False,
+        disable_proxy_for_cloud_fetch=False,
+        proxy_host="",
+        proxy_port=0,
+        proxy_uid="",
+        proxy_pwd=""
     )
