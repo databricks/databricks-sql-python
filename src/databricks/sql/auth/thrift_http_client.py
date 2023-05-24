55609475bc0e8c9f1b622fd37b5bdceacad5742e
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2023-05-24 14:55:05 -0500

    Copy THttpClient __init__ method into pysql's subclass
    I ran test_create_insert_drop_table_core to prove that the e2e tests
    still function. Meaning that this step didn't break anything.
    
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- src/databricks/sql/auth/thrift_http_client.py
+++ src/databricks/sql/auth/thrift_http_client.py
@@ -8,6 +8,11 @@ import urllib.parse, six, base64
 
 logger = logging.getLogger(__name__)
 
+import ssl
+import warnings
+from six.moves import http_client
+from io import BytesIO
+
 
 class THttpClient(thrift.transport.THttpClient.THttpClient):
     def __init__(
@@ -21,9 +26,53 @@ class THttpClient(thrift.transport.THttpClient.THttpClient):
         key_file=None,
         ssl_context=None,
     ):
-        super().__init__(
-            uri_or_host, port, path, cafile, cert_file, key_file, ssl_context
-        )
+        if port is not None:
+            warnings.warn(
+                "Please use the THttpClient('http{s}://host:port/path') constructor",
+                DeprecationWarning,
+                stacklevel=2)
+            self.host = uri_or_host
+            self.port = port
+            assert path
+            self.path = path
+            self.scheme = 'http'
+        else:
+            parsed = urllib.parse.urlparse(uri_or_host)
+            self.scheme = parsed.scheme
+            assert self.scheme in ('http', 'https')
+            if self.scheme == 'http':
+                self.port = parsed.port or http_client.HTTP_PORT
+            elif self.scheme == 'https':
+                self.port = parsed.port or http_client.HTTPS_PORT
+                self.certfile = cert_file
+                self.keyfile = key_file
+                self.context = ssl.create_default_context(cafile=cafile) if (cafile and not ssl_context) else ssl_context
+            self.host = parsed.hostname
+            self.path = parsed.path
+            if parsed.query:
+                self.path += '?%s' % parsed.query
+        try:
+            proxy = urllib.request.getproxies()[self.scheme]
+        except KeyError:
+            proxy = None
+        else:
+            if urllib.request.proxy_bypass(self.host):
+                proxy = None
+        if proxy:
+            parsed = urllib.parse.urlparse(proxy)
+            self.realhost = self.host
+            self.realport = self.port
+            self.host = parsed.hostname
+            self.port = parsed.port
+            self.proxy_auth = self.basic_proxy_auth_header(parsed)
+        else:
+            self.realhost = self.realport = self.proxy_auth = None
+        self.__wbuf = BytesIO()
+        self.__http = None
+        self.__http_response = None
+        self.__timeout = None
+        self.__custom_headers = None
+
         self.__auth_provider = auth_provider
 
     def setCustomHeaders(self, headers: Dict[str, str]):
