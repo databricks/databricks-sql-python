32611375bf32e14a34cd0a27b6e2bfb737b561e1
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2023-05-26 17:16:00 -0500

    Drain connection before closing
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- src/databricks/sql/auth/thrift_http_client.py
+++ src/databricks/sql/auth/thrift_http_client.py
@@ -97,6 +97,7 @@ class THttpClient(thrift.transport.THttpClient.THttpClient):
 
     def close(self):
         """This is a no-op because HTTP(S)ConnectionPool handles connection life-cycle"""
+        self.__resp.drain_conn()
         self.__resp.release_conn()
         self.__resp = None
 
