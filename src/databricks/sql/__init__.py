c351b57f1449def33722effd9a9a4ea26b3fa6cc
Jesse <jesse.whitehouse@databricks.com>
2023-06-26 16:45:20 -0500

    Bump to version 2.7.0 (#161)
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- src/databricks/sql/__init__.py
+++ src/databricks/sql/__init__.py
@@ -28,7 +28,7 @@ DATETIME = DBAPITypeObject("timestamp")
 DATE = DBAPITypeObject("date")
 ROWID = DBAPITypeObject()
 
-__version__ = "2.6.2"
+__version__ = "2.7.0"
 USER_AGENT_NAME = "PyDatabricksSqlConnector"
 
 # These two functions are pyhive legacy
