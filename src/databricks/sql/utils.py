e8fc63be73e671a26a7d96a142e717b24f308623
mattdeekay <11141331+mattdeekay@users.noreply.github.com>
2023-07-05 11:03:29 -0700

    Cloud fetch queue and integration (#151)
    * Cloud fetch queue and integration
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    * Enable cloudfetch with direct results
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    * Typing and style changes
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    * Client-settable max_download_threads
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    * Docstrings and comments
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    * Increase default buffer size bytes to 104857600
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    * Move max_download_threads to kwargs of ThriftBackend, fix unit tests
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    * Fix tests: staticmethod make_arrow_table mock not callable
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    * cancel_futures in shutdown() only available in python >=3.9.0
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    * Black linting
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    * Fix typing errors
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>

--- src/databricks/sql/utils.py
+++ src/databricks/sql/utils.py
@@ -1,16 +1,94 @@
+from abc import ABC, abstractmethod
 from collections import namedtuple, OrderedDict
 from collections.abc import Iterable
-import datetime, decimal
+from decimal import Decimal
+import datetime
+import decimal
 from enum import Enum
-from typing import Dict
+import lz4.frame
+from typing import Dict, List, Union, Any
 import pyarrow
 
-from databricks.sql import exc
+from databricks.sql import exc, OperationalError
+from databricks.sql.cloudfetch.download_manager import ResultFileDownloadManager
+from databricks.sql.thrift_api.TCLIService.ttypes import (
+    TSparkArrowResultLink,
+    TSparkRowSetType,
+    TRowSet,
+)
+
+BIT_MASKS = [1, 2, 4, 8, 16, 32, 64, 128]
 
 
-class ArrowQueue:
+class ResultSetQueue(ABC):
+    @abstractmethod
+    def next_n_rows(self, num_rows: int) -> pyarrow.Table:
+        pass
+
+    @abstractmethod
+    def remaining_rows(self) -> pyarrow.Table:
+        pass
+
+
+class ResultSetQueueFactory(ABC):
+    @staticmethod
+    def build_queue(
+        row_set_type: TSparkRowSetType,
+        t_row_set: TRowSet,
+        arrow_schema_bytes: bytes,
+        max_download_threads: int,
+        lz4_compressed: bool = True,
+        description: List[List[Any]] = None,
+    ) -> ResultSetQueue:
+        """
+        Factory method to build a result set queue.
+
+        Args:
+            row_set_type (enum): Row set type (Arrow, Column, or URL).
+            t_row_set (TRowSet): Result containing arrow batches, columns, or cloud fetch links.
+            arrow_schema_bytes (bytes): Bytes representing the arrow schema.
+            lz4_compressed (bool): Whether result data has been lz4 compressed.
+            description (List[List[Any]]): Hive table schema description.
+            max_download_threads (int): Maximum number of downloader thread pool threads.
+
+        Returns:
+            ResultSetQueue
+        """
+        if row_set_type == TSparkRowSetType.ARROW_BASED_SET:
+            arrow_table, n_valid_rows = convert_arrow_based_set_to_arrow_table(
+                t_row_set.arrowBatches, lz4_compressed, arrow_schema_bytes
+            )
+            converted_arrow_table = convert_decimals_in_arrow_table(
+                arrow_table, description
+            )
+            return ArrowQueue(converted_arrow_table, n_valid_rows)
+        elif row_set_type == TSparkRowSetType.COLUMN_BASED_SET:
+            arrow_table, n_valid_rows = convert_column_based_set_to_arrow_table(
+                t_row_set.columns, description
+            )
+            converted_arrow_table = convert_decimals_in_arrow_table(
+                arrow_table, description
+            )
+            return ArrowQueue(converted_arrow_table, n_valid_rows)
+        elif row_set_type == TSparkRowSetType.URL_BASED_SET:
+            return CloudFetchQueue(
+                arrow_schema_bytes,
+                start_row_offset=t_row_set.startRowOffset,
+                result_links=t_row_set.resultLinks,
+                lz4_compressed=lz4_compressed,
+                description=description,
+                max_download_threads=max_download_threads,
+            )
+        else:
+            raise AssertionError("Row set type is not valid")
+
+
+class ArrowQueue(ResultSetQueue):
     def __init__(
-        self, arrow_table: pyarrow.Table, n_valid_rows: int, start_row_index: int = 0
+        self,
+        arrow_table: pyarrow.Table,
+        n_valid_rows: int,
+        start_row_index: int = 0,
     ):
         """
         A queue-like wrapper over an Arrow table
@@ -38,6 +116,119 @@ class ArrowQueue:
         )
         self.cur_row_index += slice.num_rows
         return slice
+
+
+class CloudFetchQueue(ResultSetQueue):
+    def __init__(
+        self,
+        schema_bytes,
+        max_download_threads: int,
+        start_row_offset: int = 0,
+        result_links: List[TSparkArrowResultLink] = None,
+        lz4_compressed: bool = True,
+        description: List[List[Any]] = None,
+    ):
+        """
+        A queue-like wrapper over CloudFetch arrow batches.
+
+        Attributes:
+            schema_bytes (bytes): Table schema in bytes.
+            max_download_threads (int): Maximum number of downloader thread pool threads.
+            start_row_offset (int): The offset of the first row of the cloud fetch links.
+            result_links (List[TSparkArrowResultLink]): Links containing the downloadable URL and metadata.
+            lz4_compressed (bool): Whether the files are lz4 compressed.
+            description (List[List[Any]]): Hive table schema description.
+        """
+        self.schema_bytes = schema_bytes
+        self.max_download_threads = max_download_threads
+        self.start_row_index = start_row_offset
+        self.result_links = result_links
+        self.lz4_compressed = lz4_compressed
+        self.description = description
+
+        self.download_manager = ResultFileDownloadManager(
+            self.max_download_threads, self.lz4_compressed
+        )
+        self.download_manager.add_file_links(result_links)
+
+        self.table = self._create_next_table()
+        self.table_row_index = 0
+
+    def next_n_rows(self, num_rows: int) -> pyarrow.Table:
+        """
+        Get up to the next n rows of the cloud fetch Arrow dataframes.
+
+        Args:
+            num_rows (int): Number of rows to retrieve.
+
+        Returns:
+            pyarrow.Table
+        """
+        if not self.table:
+            # Return empty pyarrow table to cause retry of fetch
+            return self._create_empty_table()
+        results = self.table.slice(0, 0)
+        while num_rows > 0 and self.table:
+            # Get remaining of num_rows or the rest of the current table, whichever is smaller
+            length = min(num_rows, self.table.num_rows - self.table_row_index)
+            table_slice = self.table.slice(self.table_row_index, length)
+            results = pyarrow.concat_tables([results, table_slice])
+            self.table_row_index += table_slice.num_rows
+
+            # Replace current table with the next table if we are at the end of the current table
+            if self.table_row_index == self.table.num_rows:
+                self.table = self._create_next_table()
+                self.table_row_index = 0
+            num_rows -= table_slice.num_rows
+        return results
+
+    def remaining_rows(self) -> pyarrow.Table:
+        """
+        Get all remaining rows of the cloud fetch Arrow dataframes.
+
+        Returns:
+            pyarrow.Table
+        """
+        if not self.table:
+            # Return empty pyarrow table to cause retry of fetch
+            return self._create_empty_table()
+        results = self.table.slice(0, 0)
+        while self.table:
+            table_slice = self.table.slice(
+                self.table_row_index, self.table.num_rows - self.table_row_index
+            )
+            results = pyarrow.concat_tables([results, table_slice])
+            self.table_row_index += table_slice.num_rows
+            self.table = self._create_next_table()
+            self.table_row_index = 0
+        return results
+
+    def _create_next_table(self) -> Union[pyarrow.Table, None]:
+        # Create next table by retrieving the logical next downloaded file, or return None to signal end of queue
+        downloaded_file = self.download_manager.get_next_downloaded_file(
+            self.start_row_index
+        )
+        if not downloaded_file:
+            # None signals no more Arrow tables can be built from the remaining handlers if any remain
+            return None
+        arrow_table = create_arrow_table_from_arrow_file(
+            downloaded_file.file_bytes, self.description
+        )
+
+        # The server rarely prepares the exact number of rows requested by the client in cloud fetch.
+        # Subsequently, we drop the extraneous rows in the last file if more rows are retrieved than requested
+        if arrow_table.num_rows > downloaded_file.row_count:
+            self.start_row_index += downloaded_file.row_count
+            return arrow_table.slice(0, downloaded_file.row_count)
+
+        # At this point, whether the file has extraneous rows or not, the arrow table should have the correct num rows
+        assert downloaded_file.row_count == arrow_table.num_rows
+        self.start_row_index += arrow_table.num_rows
+        return arrow_table
+
+    def _create_empty_table(self) -> pyarrow.Table:
+        # Create a 0-row table with just the schema bytes
+        return create_arrow_table_from_arrow_file(self.schema_bytes, self.description)
 
 
 ExecuteResponse = namedtuple(
@@ -183,3 +374,99 @@ class ParamEscaper:
 
 def inject_parameters(operation: str, parameters: Dict[str, str]):
     return operation % parameters
+
+
+def create_arrow_table_from_arrow_file(file_bytes: bytes, description) -> pyarrow.Table:
+    arrow_table = convert_arrow_based_file_to_arrow_table(file_bytes)
+    return convert_decimals_in_arrow_table(arrow_table, description)
+
+
+def convert_arrow_based_file_to_arrow_table(file_bytes: bytes):
+    try:
+        return pyarrow.ipc.open_stream(file_bytes).read_all()
+    except Exception as e:
+        raise RuntimeError("Failure to convert arrow based file to arrow table", e)
+
+
+def convert_arrow_based_set_to_arrow_table(arrow_batches, lz4_compressed, schema_bytes):
+    ba = bytearray()
+    ba += schema_bytes
+    n_rows = 0
+    for arrow_batch in arrow_batches:
+        n_rows += arrow_batch.rowCount
+        ba += (
+            lz4.frame.decompress(arrow_batch.batch)
+            if lz4_compressed
+            else arrow_batch.batch
+        )
+    arrow_table = pyarrow.ipc.open_stream(ba).read_all()
+    return arrow_table, n_rows
+
+
+def convert_decimals_in_arrow_table(table, description) -> pyarrow.Table:
+    for (i, col) in enumerate(table.itercolumns()):
+        if description[i][1] == "decimal":
+            decimal_col = col.to_pandas().apply(
+                lambda v: v if v is None else Decimal(v)
+            )
+            precision, scale = description[i][4], description[i][5]
+            assert scale is not None
+            assert precision is not None
+            # Spark limits decimal to a maximum scale of 38,
+            # so 128 is guaranteed to be big enough
+            dtype = pyarrow.decimal128(precision, scale)
+            col_data = pyarrow.array(decimal_col, type=dtype)
+            field = table.field(i).with_type(dtype)
+            table = table.set_column(i, field, col_data)
+    return table
+
+
+def convert_column_based_set_to_arrow_table(columns, description):
+    arrow_table = pyarrow.Table.from_arrays(
+        [_convert_column_to_arrow_array(c) for c in columns],
+        # Only use the column names from the schema, the types are determined by the
+        # physical types used in column based set, as they can differ from the
+        # mapping used in _hive_schema_to_arrow_schema.
+        names=[c[0] for c in description],
+    )
+    return arrow_table, arrow_table.num_rows
+
+
+def _convert_column_to_arrow_array(t_col):
+    """
+    Return a pyarrow array from the values in a TColumn instance.
+    Note that ColumnBasedSet has no native support for complex types, so they will be converted
+    to strings server-side.
+    """
+    field_name_to_arrow_type = {
+        "boolVal": pyarrow.bool_(),
+        "byteVal": pyarrow.int8(),
+        "i16Val": pyarrow.int16(),
+        "i32Val": pyarrow.int32(),
+        "i64Val": pyarrow.int64(),
+        "doubleVal": pyarrow.float64(),
+        "stringVal": pyarrow.string(),
+        "binaryVal": pyarrow.binary(),
+    }
+    for field in field_name_to_arrow_type.keys():
+        wrapper = getattr(t_col, field)
+        if wrapper:
+            return _create_arrow_array(wrapper, field_name_to_arrow_type[field])
+
+    raise OperationalError("Empty TColumn instance {}".format(t_col))
+
+
+def _create_arrow_array(t_col_value_wrapper, arrow_type):
+    result = t_col_value_wrapper.values
+    nulls = t_col_value_wrapper.nulls  # bitfield describing which values are null
+    assert isinstance(nulls, bytes)
+
+    # The number of bits in nulls can be both larger or smaller than the number of
+    # elements in result, so take the minimum of both to iterate over.
+    length = min(len(result), len(nulls) * 8)
+
+    for i in range(length):
+        if nulls[i >> 3] & BIT_MASKS[i & 0x7]:
+            result[i] = None
+
+    return pyarrow.array(result, type=arrow_type)
