74836074bb558fcba3b2d77699df25735a1d7bac
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2023-09-20 13:12:46 -0400

    Typo in docstring
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- tests/sqlalchemy_dialect_compliance/conftest.py
+++ tests/sqlalchemy_dialect_compliance/conftest.py
@@ -4,7 +4,7 @@ import pytest
 registry.register("databricks", "databricks.sqlalchemy", "DatabricksDialect")
 # sqlalchemy's dialect-testing machinery wants an entry like this.
 # This seems to be based around dialects maybe having multiple drivers
-# and wanting to test drover-specific URLs, but doesn't seem to make
+# and wanting to test driver-specific URLs, but doesn't seem to make
 # much sense for dialects with only one driver.
 registry.register("databricks.databricks", "databricks.sqlalchemy", "DatabricksDialect")
 
