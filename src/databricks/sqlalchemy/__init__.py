c6fea522c2753ec24aae45ddcbe91f184e38a154
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2023-09-22 15:57:35 -0400

    WIP: update default paramstyle to be compatible with DBR (ROLL ME BACK PLEASE)
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- src/databricks/sqlalchemy/__init__.py
+++ src/databricks/sqlalchemy/__init__.py
@@ -82,6 +82,7 @@ class DatabricksDialect(default.DefaultDialect):
     supports_native_decimal: bool = True
     supports_sane_rowcount: bool = False
     non_native_boolean_check_constraint: bool = False
+    default_paramstyle: str = "qmark"
 
     @classmethod
     def dbapi(cls):
