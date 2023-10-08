aa1cae3ebf9694b600dd4d718892afae5384bfbc
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2023-10-08 17:06:54 -0400

    Stop skipping NumericTest
    I added exclusions to requirements.py for features which Databricks doesn't
    natively support. These tests apply to the generic (camelcase) Numeric() type.
    
    Therefore the tests are applicable to the DECIMAL Databricks data type. Some of
    these features _will_ work for Databricks' FLOAT type. Those tests marked as skipped
    in the below listing are skipped because of the exclusions called out in requirements.py
    
    test_suite.py::NumericTest_databricks+databricks::test_decimal_coerce_round_trip PASSED
    test_suite.py::NumericTest_databricks+databricks::test_decimal_coerce_round_trip_w_cast PASSED
    test_suite.py::NumericTest_databricks+databricks::test_enotation_decimal SKIPPED ('test/test_suite.py::NumericTest_databricks+databricks::test_enotation_decimal (call)' : marked as skip)
    test_suite.py::NumericTest_databricks+databricks::test_enotation_decimal_large SKIPPED ('test/test_suite.py::NumericTest_databricks+databricks::test_enotation_decimal_large (call)' : marked as skip)
    test_suite.py::NumericTest_databricks+databricks::test_float_as_decimal PASSED
    test_suite.py::NumericTest_databricks+databricks::test_float_as_float PASSED
    test_suite.py::NumericTest_databricks+databricks::test_float_coerce_round_trip SKIPPED ('test/test_suite.py::NumericTest_databricks+databricks::test_float_coerce_round_trip (call)' : marked as skip)
    test_suite.py::NumericTest_databricks+databricks::test_float_custom_scale SKIPPED ('test/test_suite.py::NumericTest_databricks+databricks::test_float_custom_scale (call)' : marked as skip)
    test_suite.py::NumericTest_databricks+databricks::test_float_is_not_numeric[Double] PASSED
    test_suite.py::NumericTest_databricks+databricks::test_float_is_not_numeric[Float] PASSED
    test_suite.py::NumericTest_databricks+databricks::test_infinity_floats PASSED
    test_suite.py::NumericTest_databricks+databricks::test_many_significant_digits SKIPPED ('test/test_suite.py::NumericTest_databricks+databricks::test_many_significant_digits (call)' : marked as skip)
    test_suite.py::NumericTest_databricks+databricks::test_numeric_as_decimal PASSED
    test_suite.py::NumericTest_databricks+databricks::test_numeric_as_float PASSED
    test_suite.py::NumericTest_databricks+databricks::test_numeric_no_decimal PASSED
    test_suite.py::NumericTest_databricks+databricks::test_numeric_null_as_decimal PASSED
    test_suite.py::NumericTest_databricks+databricks::test_numeric_null_as_float PASSED
    test_suite.py::NumericTest_databricks+databricks::test_precision_decimal PASSED
    test_suite.py::NumericTest_databricks+databricks::test_render_literal_float PASSED
    test_suite.py::NumericTest_databricks+databricks::test_render_literal_numeric PASSED
    test_suite.py::NumericTest_databricks+databricks::test_render_literal_numeric_asfloat PASSED
    
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- src/databricks/sqlalchemy/requirements.py
+++ src/databricks/sqlalchemy/requirements.py
@@ -77,5 +77,56 @@ class Requirements(sqlalchemy.testing.requirements.SuiteRequirements):
         """
 
         return sqlalchemy.testing.exclusions.open()
+    
+    @property
+    def precision_generic_float_type(self):
+        """target backend will return native floating point numbers with at
+        least seven decimal places when using the generic Float type.
+
+        Databricks sometimes only returns six digits of precision for the generic Float type
+        """
+        return sqlalchemy.testing.exclusions.closed()
+    
+    @property
+    def literal_float_coercion(self):
+        """target backend will return the exact float value 15.7563
+        with only four significant digits from this statement:
+
+        SELECT :param
+
+        where :param is the Python float 15.7563
+
+        i.e. it does not return 15.75629997253418
+
+        Without additional work, Databricks returns 15.75629997253418
+        This is a potential area where we could override the Float literal processor.
+        Will leave to a PM to decide if we should do so.
+        """
+        return sqlalchemy.testing.exclusions.closed()
+    
+    @property
+    def precision_numerics_enotation_large(self):
+        """target backend supports Decimal() objects using E notation
+        to represent very large values.
+        
+        Databricks supports E notation for FLOAT data types but not for DECIMAL types,
+        which is the underlying data type SQLAlchemy uses for Numeric() types.
+
+        """
+        return sqlalchemy.testing.exclusions.closed()
+    
+    @property
+    def infinity_floats(self):
+        """The Float type can persist and load float('inf'), float('-inf')."""
+
+        return sqlalchemy.testing.exclusions.open()
+    
+    @property
+    def precision_numerics_retains_significant_digits(self):
+        """A precision numeric type will return empty significant digits,
+        i.e. a value such as 10.000 will come back in Decimal form with
+        the .000 maintained."""
+
+        return sqlalchemy.testing.exclusions.open()
 
 
