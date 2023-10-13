f3657ee83732e33d624690cd6f73697fcaabb855
Jesse <jesse.whitehouse@databricks.com>
2023-10-13 19:35:24 -0400

    SQLAlchemy 2: implement and refactor schema reflection methods (#249)
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>

--- src/databricks/sqlalchemy/test_local/test_parsing.py
+++ src/databricks/sqlalchemy/test_local/test_parsing.py
@@ -1,7 +1,9 @@
 import pytest
-from databricks.sqlalchemy.utils import (
+from databricks.sqlalchemy._parse import (
     extract_identifiers_from_string,
     extract_identifier_groups_from_string,
+    extract_three_level_identifier_from_constraint_string,
+    build_fk_dict
 )
 
 
@@ -36,3 +38,29 @@ def test_extract_identifer_batches(input, expected):
     assert (
         extract_identifier_groups_from_string(input) == expected
     ), "Failed to extract identifier groups from string"
+
+def test_extract_3l_namespace_from_constraint_string():
+
+    input = "FOREIGN KEY (`parent_user_id`) REFERENCES `main`.`pysql_dialect_compliance`.`users` (`user_id`)"
+    expected = {
+        "catalog": "main",
+        "schema": "pysql_dialect_compliance",
+        "table": "users"
+    }
+
+    assert extract_three_level_identifier_from_constraint_string(input) == expected, "Failed to extract 3L namespace from constraint string"
+
+@pytest.mark.parametrize("schema", [None, "some_schema"])
+def test_build_fk_dict(schema):
+    fk_constraint_string = "FOREIGN KEY (`parent_user_id`) REFERENCES `main`.`some_schema`.`users` (`user_id`)"
+
+    result = build_fk_dict("some_fk_name", fk_constraint_string, schema_name=schema)
+
+    assert result == {
+        "name": "some_fk_name",
+        "constrained_columns": ["parent_user_id"],
+        "referred_schema": schema,
+        "referred_table": "users",
+        "referred_columns": ["user_id"],
+    }
+
