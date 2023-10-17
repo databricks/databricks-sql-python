3bfa7a97afe897cdc73c58864431fe1ebdc7c120
Jacky Hu <jacky.hu@databricks.com>
2023-10-17 10:36:11 -0700

    Add GocCloud domain into AWS domains
    Signed-off-by: Jacky Hu <jacky.hu@databricks.com>

--- src/databricks/sql/auth/endpoint.py
+++ src/databricks/sql/auth/endpoint.py
@@ -23,7 +23,7 @@ class CloudType(Enum):
     AZURE = "azure"
 
 
-DATABRICKS_AWS_DOMAINS = [".cloud.databricks.com", ".dev.databricks.com"]
+DATABRICKS_AWS_DOMAINS = [".cloud.databricks.com", ".dev.databricks.com", ".cloud.databricks.us"]
 DATABRICKS_AZURE_DOMAINS = [
     ".azuredatabricks.net",
     ".databricks.azure.cn",
