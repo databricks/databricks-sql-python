ad5c342f21f6457bc39ae2b883b66d2b5414226c
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2023-10-22 23:44:06 -0400

    Document test layout in a new README
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- src/databricks/sqlalchemy/test/test_suite.py
+++ src/databricks/sqlalchemy/test/test_suite.py
@@ -1,29 +1,16 @@
+"""
+The order of these imports is important. Test cases are imported first from SQLAlchemy,
+then are overridden by our local skip markers in _regression, _unsupported, and _future.
+"""
+
 # type: ignore
+# fmt: off
 from sqlalchemy.testing.suite import *
 from databricks.sqlalchemy.test._regression import *
 from databricks.sqlalchemy.test._unsupported import *
 from databricks.sqlalchemy.test._future import *
 
-# Test definitions are found here:
-# https://github.com/sqlalchemy/sqlalchemy/tree/main/lib/sqlalchemy/testing/suite
 
-
-# Per the instructions for dialect authors, tests should be skippable based on
-# dialect limitations defined in requirements.py. However, we found that these
-# definitions are not universally honoured by the SQLAlchemy test runner so we
-# opt to manually delete them from the test suite for the time-being. This makes
-# it obvious what areas of dialect compliance we have not evaluated. The next
-# step towards dialect compliance is to review each of these and document exactly
-# which methods should or should not work. This can be done by removing the corr-
-# esponding skip marker and then running the test.
-
-# If we find tests that are skippable for a documented reason, we can call these
-# out directly in the way suggested by SQLAlchemy's document for dialect authors:
-#
-#   > In the case that the decorators are not covering a particular test, a test
-#   > can also be directly modified or bypassed.
-#
-# See further: https://github.com/sqlalchemy/sqlalchemy/blob/rel_1_4_48/README.dialects.rst
 
 
 
