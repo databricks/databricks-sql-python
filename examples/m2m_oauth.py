3e16c5d8bf279064f26456f7d4031c7d4f72bacf
Jacky Hu <jacky.hu@databricks.com>
2023-10-30 11:34:32 -0700

    Add OAuth M2M example
    Signed-off-by: Jacky Hu <jacky.hu@databricks.com>

---
+++ examples/m2m_oauth.py
@@ -0,0 +1,31 @@
+import os
+
+from databricks.sdk.core import oauth_service_principal, Config
+from databricks import sql
+
+server_hostname = os.getenv("DATABRICKS_SERVER_HOSTNAME")
+
+
+def credential_provider():
+    config = Config(
+        host=f"https://{server_hostname}",
+        # Service Principal UUID
+        client_id=os.getenv("DATABRICKS_CLIENT_ID"),
+        # Service Principal Secret
+        client_secret=os.getenv("DATABRICKS_CLIENT_SECRET"))
+    return oauth_service_principal(config)
+
+
+with sql.connect(
+        server_hostname=server_hostname,
+        http_path=os.getenv("DATABRICKS_HTTP_PATH"),
+        credentials_provider=credential_provider) as connection:
+    for x in range(1, 100):
+        cursor = connection.cursor()
+        cursor.execute('SELECT 1+1')
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+        cursor.close()
+
+    connection.close()
