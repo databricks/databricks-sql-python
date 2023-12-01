9e963a04478961b0d1eec56bbc4fe447e702ba24
Jesse <jesse.whitehouse@databricks.com>
2023-12-01 12:06:10 -0500

    Prepared 3.0.1 release (#297)
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- src/databricks/sql/__init__.py
+++ src/databricks/sql/__init__.py
@@ -29,7 +29,7 @@ DATETIME = DBAPITypeObject("timestamp")
 DATE = DBAPITypeObject("date")
 ROWID = DBAPITypeObject()
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 USER_AGENT_NAME = "PyDatabricksSqlConnector"
 
 # These two functions are pyhive legacy
