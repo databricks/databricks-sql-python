9d7db92496f2701defaed497f7ddc25f1f7c5e15
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2023-12-21 11:59:20 -0500

    Use pytest
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- tests/unit/test_init_file.py
+++ tests/unit/test_init_file.py
@@ -1,8 +1,7 @@
 import hashlib
-import unittest
 
 
-class InitFileTests(unittest.TestCase):
+class TestInitFile:
     """
     Micro test to confirm the contents of `databricks/__init__.py` does not change.
 
@@ -10,15 +9,11 @@ class InitFileTests(unittest.TestCase):
     """
 
     def test_init_file_contents(self):
-        with open('src/databricks/__init__.py') as f:
+        with open("src/databricks/__init__.py") as f:
             init_file_contents = f.read()
 
         # This hash is the expected hash of the contents of `src/databricks/__init__.py`.
         # It must not change, or else parallel package installation may lead to clobbered and invalid files.
-        expected_sha1 = '2772edbf52e517542acf8c039479c4b57b6ca2cd'
-        actual_sha1 = hashlib.sha1(init_file_contents.encode('utf-8')).hexdigest()
-        self.assertEqual(expected_sha1, actual_sha1)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        expected_sha1 = "2772edbf52e517542acf8c039479c4b57b6ca2cd"
+        actual_sha1 = hashlib.sha1(init_file_contents.encode("utf-8")).hexdigest()
+        assert expected_sha1 == actual_sha1
