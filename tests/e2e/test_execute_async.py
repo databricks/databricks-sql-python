e966b1c1e83f05888d0932998775d4e0b11d6b8f
Jesse Whitehouse <jesse.whitehouse@databricks.com>
2024-01-08 16:57:56 -0500

    Add tests and update fixtures to evaluate behaviour when a query execution takes longer than 5 seconds and therefore the AsyncExecution returned by `execute_async` doesn't have a result yet.
    Signed-off-by: Jesse Whitehouse <jesse.whitehouse@databricks.com>

--- tests/e2e/test_execute_async.py
+++ tests/e2e/test_execute_async.py
@@ -10,11 +10,23 @@ import time
 
 import threading
 
-LONG_RUNNING_QUERY = """
+BASE_LONG_QUERY = """
 SELECT SUM(A.id - B.id)
-FROM range(1000000000) A CROSS JOIN range(100000000) B 
+FROM range({val}) A CROSS JOIN range({val}) B 
 GROUP BY (A.id - B.id)
 """
+GT_ONE_MINUTE_VALUE = 100000000
+
+# Arrived at this value through some manual testing on a serverless SQL warehouse
+# The goal here is to have a query that takes longer than five seconds (therefore bypassing directResults)
+# but not so long that I can't attempt to fetch its results in a reasonable amount of time
+GT_FIVE_SECONDS_VALUE = 75000
+
+LONG_RUNNING_QUERY = BASE_LONG_QUERY.format(val=GT_ONE_MINUTE_VALUE)
+LONG_ISH_QUERY = BASE_LONG_QUERY.format(val=GT_FIVE_SECONDS_VALUE)
+
+# This query should always return in < 5 seconds and therefore should be a direct result
+DIRECT_RESULTS_QUERY = "select :param `col`"
 
 
 class TestExecuteAsync(PySQLPytestTestCase):
@@ -28,12 +40,19 @@ class TestExecuteAsync(PySQLPytestTestCase):
             # cancellation is idempotent
             ae.cancel()
 
+    @pytest.fixture
+    def long_ish_ae(self, scope="function") -> AsyncExecution:
+        """Start a long-running query so we can make assertions about it."""
+        with self.connection() as conn:
+            ae = conn.execute_async(LONG_ISH_QUERY)
+            yield ae
+
     def test_execute_async(self):
         """This is a WIP test of the basic API defined in PECO-1263"""
         # This is taken directly from the design doc
 
         with self.connection() as conn:
-            ae = conn.execute_async("select :param `col`", {"param": 1})
+            ae = conn.execute_async(DIRECT_RESULTS_QUERY, {"param": 1})
             while ae.is_running:
                 ae.poll_for_status()
                 time.sleep(1)
@@ -41,6 +60,15 @@ class TestExecuteAsync(PySQLPytestTestCase):
             result = ae.get_result().fetchone()
 
         assert result.col == 1
+
+    def test_direct_results_query_canary(self):
+        """This test verifies that on the current endpoint, the DIRECT_RESULTS_QUERY returned a thrift operation state
+        other than FINISHED_STATE. If this test fails, it means the SQL warehouse got slower at executing this query
+        """
+
+        with self.connection() as conn:
+            ae = conn.execute_async(DIRECT_RESULTS_QUERY, {"param": 1})
+            assert not ae.is_running
 
     def test_cancel_running_query(self, long_running_ae: AsyncExecution):
         long_running_ae.cancel()
@@ -83,9 +111,29 @@ class TestExecuteAsync(PySQLPytestTestCase):
         long_running_ae.poll_for_status()
         assert long_running_ae.status == AsyncExecutionStatus.CANCELED
 
-    def test_staging_operation(self):
-        """We need to test what happens with a staging operation since this query won't have a result set
-        that user needs. It could be sufficient to notify users that they shouldn't use this API for staging/volumes
-        queries...
+    def test_long_ish_query_canary(self, long_ish_ae: AsyncExecution):
+        """This test verifies that on the current endpoint, the LONG_ISH_QUERY requires
+        at least one poll_for_status call before it is finished. If this test fails, it means
+        the SQL warehouse got faster at executing this query and we should increment the value
+        of GT_FIVE_SECONDS_VALUE
+
+        It would be easier to do this if Databricks SQL had a SLEEP() function :/
         """
-        assert False
+
+        poll_count = 0
+        while long_ish_ae.is_running:
+            time.sleep(1)
+            long_ish_ae.poll_for_status()
+            poll_count += 1
+
+        assert poll_count > 0
+
+    def test_get_async_execution_and_get_results_without_direct_results(
+        self, long_ish_ae: AsyncExecution
+    ):
+        while long_ish_ae.is_running:
+            time.sleep(1)
+            long_ish_ae.poll_for_status()
+
+        result = long_ish_ae.get_result().fetchone()
+        assert len(result) == 1
