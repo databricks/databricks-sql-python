95fe8b2f50e1c571e30160af9b452661df1536a1
Jacky Hu <jacky.hu@databricks.com>
2024-02-16 13:20:51 -0800

    Bump to version 3.1.0 (#358)
    Signed-off-by: Jacky Hu <jacky.hu@databricks.com>
    Signed-off-by: varun-edachali-dbx <varun.edachali@databricks.com>

--- src/databricks/sql/__init__.py
+++ src/databricks/sql/__init__.py
@@ -62,7 +62,7 @@ DATETIME = DBAPITypeObject("timestamp")
 DATE = DBAPITypeObject("date")
 ROWID = DBAPITypeObject()
 
-__version__ = "3.0.3"
+__version__ = "3.1.0"
 USER_AGENT_NAME = "PyDatabricksSqlConnector"
 
 # These two functions are pyhive legacy
