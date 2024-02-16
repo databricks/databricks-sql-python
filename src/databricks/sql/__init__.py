d46b5011cffe3e01f19fa6fd06355d9bb2a8eb72
Andre Furlan <andre.furlan@databricks.com>
2024-02-16 10:46:18 -0800

    bump version to 2.9.4b1 (#359)
    Signed-off-by: Andre Furlan <andre.furlan@databricks.com>

--- src/databricks/sql/__init__.py
+++ src/databricks/sql/__init__.py
@@ -60,7 +60,7 @@ DATETIME = DBAPITypeObject("timestamp")
 DATE = DBAPITypeObject("date")
 ROWID = DBAPITypeObject()
 
-__version__ = "2.9.3"
+__version__ = "2.9.4b1"
 USER_AGENT_NAME = "PyDatabricksSqlConnector"
 
 # These two functions are pyhive legacy
