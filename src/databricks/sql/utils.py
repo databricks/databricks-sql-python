f8d26303aeaa1c04154781ba810891d046fbb63e
Levko Kravets <levko.ne@gmail.com>
2024-07-11 11:10:56 +0300

    [PECO-1751] Refactor CloudFetch downloader: handle files sequentially (#405)
    * [PECO-1751] Refactor CloudFetch downloader: handle files sequentially; utilize Futures
    
    Signed-off-by: Levko Kravets <levko.ne@gmail.com>
    
    * Retry failed CloudFetch downloads
    
    Signed-off-by: Levko Kravets <levko.ne@gmail.com>
    
    * Update tests
    
    Signed-off-by: Levko Kravets <levko.ne@gmail.com>
    
    ---------
    
    Signed-off-by: Levko Kravets <levko.ne@gmail.com>
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>

--- src/databricks/sql/utils.py
+++ src/databricks/sql/utils.py
@@ -169,9 +169,8 @@ class CloudFetchQueue(ResultSetQueue):
                     )
                 )
         self.download_manager = ResultFileDownloadManager(
-            self.max_download_threads, self.lz4_compressed
+            result_links or [], self.max_download_threads, self.lz4_compressed
         )
-        self.download_manager.add_file_links(result_links or [])
 
         self.table = self._create_next_table()
         self.table_row_index = 0
