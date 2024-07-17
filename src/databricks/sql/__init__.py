caa30abb29e19405eeab89036fee7f0afee53a1d
Levko Kravets <levko.ne@gmail.com>
2024-07-17 14:30:12 +0300

    Prepare relese 3.3.0
    Signed-off-by: Levko Kravets <levko.ne@gmail.com>

--- src/databricks/sql/__init__.py
+++ src/databricks/sql/__init__.py
@@ -68,7 +68,7 @@ DATETIME = DBAPITypeObject("timestamp")
 DATE = DBAPITypeObject("date")
 ROWID = DBAPITypeObject()
 
-__version__ = "3.2.0"
+__version__ = "3.3.0"
 USER_AGENT_NAME = "PyDatabricksSqlConnector"
 
 # These two functions are pyhive legacy
