c25028abf59668948500ebc5765bbe1a5c9dedb0
Gabriel <gabriel59kg@gmail.com>
2024-09-17 09:26:11 -0400

    export DatabricksDialect from top level `__init__.py`

--- src/databricks/sqlalchemy/__init__.py
+++ src/databricks/sqlalchemy/__init__.py
@@ -1,4 +1,4 @@
 from databricks.sqlalchemy.base import DatabricksDialect
 from databricks.sqlalchemy._types import TINYINT, TIMESTAMP, TIMESTAMP_NTZ
 
-__all__ = ["TINYINT", "TIMESTAMP", "TIMESTAMP_NTZ"]
+__all__ = ["DatabricksDialect", "TINYINT", "TIMESTAMP", "TIMESTAMP_NTZ"]
