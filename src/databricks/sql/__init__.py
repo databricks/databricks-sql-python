a9415756b17bfc58d29d1f593c4fc7ed475967a6
Jothi Prakash <jothi.prakash@databricks.com>
2025-01-22 16:24:48 +0530

    Bumped up to version 4.0.0 (#493)
    bumped up the version
    
    Signed-off-by: varun-edachali-dbx <varun.edachali@databricks.com>

--- src/databricks/sql/__init__.py
+++ src/databricks/sql/__init__.py
@@ -68,7 +68,7 @@ DATETIME = DBAPITypeObject("timestamp")
 DATE = DBAPITypeObject("date")
 ROWID = DBAPITypeObject()
 
-__version__ = "3.7.0"
+__version__ = "4.0.0"
 USER_AGENT_NAME = "PyDatabricksSqlConnector"
 
 # These two functions are pyhive legacy
