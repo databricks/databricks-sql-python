72b0044573ef4de868ce0037d70b40032e5fbb43
Jothi Prakash <jothi.prakash@databricks.com>
2025-02-19 17:03:36 +0530

    Support for enforcing embedded schema (#505)
    * Added the enforce schema option
    
    * Renamed the variable

--- src/databricks/sql/thrift_backend.py
+++ src/databricks/sql/thrift_backend.py
@@ -883,6 +883,7 @@ class ThriftBackend:
         use_cloud_fetch=True,
         parameters=[],
         async_op=False,
+        enforce_embedded_schema_correctness=False,
     ):
         assert session_handle is not None
 
@@ -910,6 +911,7 @@ class ThriftBackend:
             },
             useArrowNativeTypes=spark_arrow_types,
             parameters=parameters,
+            enforceEmbeddedSchemaCorrectness=enforce_embedded_schema_correctness,
         )
         resp = self.make_request(self._client.ExecuteStatement, req)
 
