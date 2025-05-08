9a6eb40579f192e01decf8acc343e0c8670f8acc
Vikrant Puppala <vikrant.puppala@databricks.com>
2025-05-08 20:22:28 +0530

    consequent changes
    Signed-off-by: Vikrant Puppala <vikrant.puppala@databricks.com>

--- tests/unit/test_thrift_backend.py
+++ tests/unit/test_thrift_backend.py
@@ -86,7 +86,7 @@ class ThriftBackendTestSuite(unittest.TestCase):
 
     def _make_type_desc(self, type):
         return ttypes.TTypeDesc(
-            types=[ttypes.TTypeEntry(ttypes.TTAllowedParameterValueEntry(type=type))]
+            types=[ttypes.TTypeEntry(primitiveEntry=ttypes.TPrimitiveTypeEntry(type=type))]
         )
 
     def _make_fake_thrift_backend(self):
@@ -510,7 +510,7 @@ class ThriftBackendTestSuite(unittest.TestCase):
                 typeDesc=ttypes.TTypeDesc(
                     types=[
                         ttypes.TTypeEntry(
-                            ttypes.TTAllowedParameterValueEntry(
+                            primitiveEntry=ttypes.TPrimitiveTypeEntry(
                                 type=ttypes.TTypeId.DECIMAL_TYPE,
                                 typeQualifiers=ttypes.TTypeQualifiers(
                                     qualifiers={
