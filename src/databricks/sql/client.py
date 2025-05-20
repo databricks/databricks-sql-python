8789e32b2d4bd7761ee8841c612be4cfb6031059
varun-edachali-dbx <varun.edachali@databricks.com>
2025-05-20 16:18:44 +0530

    use connection open property instead of long chain through session
    Signed-off-by: varun-edachali-dbx <varun.edachali@databricks.com>

--- src/databricks/sql/client.py
+++ src/databricks/sql/client.py
@@ -282,7 +282,7 @@ class Connection:
         self.close()
 
     def __del__(self):
-        if self.session.open:
+        if self.open:
             logger.debug(
                 "Closing unclosed connection for session "
                 "{}".format(self.get_session_id_hex())
@@ -331,7 +331,7 @@ class Connection:
 
         Will throw an Error if the connection has been closed.
         """
-        if not self.session.open:
+        if not self.open:
             raise Error("Cannot create cursor from closed connection")
 
         cursor = Cursor(
@@ -1437,7 +1437,7 @@ class ResultSet:
             if (
                 self.op_state != self.thrift_backend.CLOSED_OP_STATE
                 and not self.has_been_closed_server_side
-                and self.connection.session.open
+                and self.connection.open
             ):
                 self.thrift_backend.close_command(self.command_id)
         except RequestError as e:
