a5152d00b5243ed8a8cd3414a864711264c2ba4a
Sai Shree Pradhan <saishree.pradhan@databricks.com>
2025-05-23 00:31:40 +0530

    PECOBLR-86 improve logging on python driver (#556)
    * PECOBLR-86 Improve logging for debug level
    
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>
    
    * PECOBLR-86 Improve logging for debug level
    
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>
    
    * fixed format
    
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>
    
    * used lazy logging
    
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>
    
    * changed debug to error logs
    
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>
    
    * used lazy logging
    
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>
    
    ---------
    
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>
    Signed-off-by: varun-edachali-dbx <varun.edachali@databricks.com>

--- src/databricks/sql/backend/thrift_backend.py
+++ src/databricks/sql/backend/thrift_backend.py
@@ -133,6 +133,13 @@ class ThriftDatabricksClient(DatabricksClient):
         # max_download_threads
         #  Number of threads for handling cloud fetch downloads. Defaults to 10
 
+        logger.debug(
+            "ThriftBackend.__init__(server_hostname=%s, port=%s, http_path=%s)",
+            server_hostname,
+            port,
+            http_path,
+        )
+
         port = port or 443
         if kwargs.get("_connection_uri"):
             uri = kwargs.get("_connection_uri")
@@ -404,6 +411,8 @@ class ThriftDatabricksClient(DatabricksClient):
 
                 # TODO: don't use exception handling for GOS polling...
 
+                logger.error("ThriftBackend.attempt_request: HTTPError: %s", err)
+
                 gos_name = TCLIServiceClient.GetOperationStatus.__name__
                 if method.__name__ == gos_name:
                     delay_default = (
@@ -448,6 +457,7 @@ class ThriftDatabricksClient(DatabricksClient):
                     else:
                         logger.warning(log_string)
             except Exception as err:
+                logger.error("ThriftBackend.attempt_request: Exception: %s", err)
                 error = err
                 retry_delay = extract_retry_delay(attempt)
                 error_message = (
@@ -914,6 +924,12 @@ class ThriftDatabricksClient(DatabricksClient):
     ):
         assert session_handle is not None
 
+        logger.debug(
+            "ThriftBackend.execute_command(operation=%s, session_handle=%s)",
+            operation,
+            session_handle,
+        )
+
         spark_arrow_types = ttypes.TSparkArrowTypes(
             timestampAsArrow=self._use_arrow_native_timestamps,
             decimalAsArrow=self._use_arrow_native_decimals,
@@ -1150,6 +1166,7 @@ class ThriftDatabricksClient(DatabricksClient):
         return queue, resp.hasMoreRows
 
     def close_command(self, op_handle):
+        logger.debug("ThriftBackend.close_command(op_handle=%s)", op_handle)
         req = ttypes.TCloseOperationReq(operationHandle=op_handle)
         resp = self.make_request(self._client.CloseOperation, req)
         return resp.status
