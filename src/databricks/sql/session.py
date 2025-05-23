63b10c3bdefd876d077c8d6c8bf4e9b1809412a7
varun-edachali-dbx <varun.edachali@databricks.com>
2025-05-23 04:48:59 +0000

    Revert "fix: de-complicate earlier connection open logic"
    This reverts commit d6b1b196c98a6e9d8e593a88c34bbde010519ef4.
    
    Signed-off-by: varun-edachali-dbx <varun.edachali@databricks.com>

--- src/databricks/sql/session.py
+++ src/databricks/sql/session.py
@@ -29,7 +29,7 @@ class Session:
 
         This class handles all session-related behavior and communication with the backend.
         """
-        self.is_open = False
+        self.open = False
         self.host = server_hostname
         self.port = kwargs.get("_port", 443)
 
@@ -77,12 +77,13 @@ class Session:
             _use_arrow_native_complex_types=_use_arrow_native_complex_types,
             **kwargs,
         )
+
         self._open_session_resp = self.thrift_backend.open_session(
             session_configuration, catalog, schema
         )
         self._session_handle = self._open_session_resp.sessionHandle
         self.protocol_version = self.get_protocol_version(self._open_session_resp)
-        self.is_open = True
+        self.open = True
         logger.info("Successfully opened session " + str(self.get_session_id_hex()))
 
     @staticmethod
@@ -121,7 +122,7 @@ class Session:
     def close(self) -> None:
         """Close the underlying session."""
         logger.info(f"Closing session {self.get_session_id_hex()}")
-        if not self.is_open:
+        if not self.open:
             logger.debug("Session appears to have been closed already")
             return
 
@@ -142,4 +143,4 @@ class Session:
         except Exception as e:
             logger.error(f"Attempt to close session raised a local exception: {e}")
 
-        self.is_open = False
+        self.open = False
