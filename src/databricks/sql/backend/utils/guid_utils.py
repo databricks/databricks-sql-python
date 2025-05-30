26c017d72efc446de7d38dec3d5616249872fe61
varun-edachali-dbx <varun.edachali@databricks.com>
2025-05-30 16:12:43 +0000

    chore: docstring
    Signed-off-by: varun-edachali-dbx <varun.edachali@databricks.com>

--- src/databricks/sql/backend/utils/guid_utils.py
+++ src/databricks/sql/backend/utils/guid_utils.py
@@ -11,7 +11,8 @@ def guid_to_hex_id(guid: bytes) -> str:
         IN   b'\x01\xee\x1d)\xa4\x19\x1d\xb6\xa9\xc0\x8d\xf1\xfe\xbaB\xdd'
         OUT  '01ee1d29-a419-1db6-a9c0-8df1feba42dd'
 
-    If conversion to hexadecimal fails, the original bytes are returned
+    If conversion to hexadecimal fails, a string representation of the original
+    bytes is returned
     """
     try:
         this_uuid = uuid.UUID(bytes=guid)
