da5a6fe7511e927c511d61adb222b8a6a0da14d3
varun-edachali-dbx <varun.edachali@databricks.com>
2025-06-09 06:39:11 +0000

    remove excess models
    Signed-off-by: varun-edachali-dbx <varun.edachali@databricks.com>

--- src/databricks/sql/backend/sea/models/__init__.py
+++ src/databricks/sql/backend/sea/models/__init__.py
@@ -4,49 +4,19 @@ Models for the SEA (Statement Execution API) backend.
 This package contains data models for SEA API requests and responses.
 """
 
-from databricks.sql.backend.sea.models.base import (
-    ServiceError,
-    StatementStatus,
-    ExternalLink,
-    ResultData,
-    ColumnInfo,
-    ResultManifest,
-)
-
 from databricks.sql.backend.sea.models.requests import (
-    StatementParameter,
-    ExecuteStatementRequest,
-    GetStatementRequest,
-    CancelStatementRequest,
-    CloseStatementRequest,
     CreateSessionRequest,
     DeleteSessionRequest,
 )
 
 from databricks.sql.backend.sea.models.responses import (
-    ExecuteStatementResponse,
-    GetStatementResponse,
     CreateSessionResponse,
 )
 
 __all__ = [
-    # Base models
-    "ServiceError",
-    "StatementStatus",
-    "ExternalLink",
-    "ResultData",
-    "ColumnInfo",
-    "ResultManifest",
     # Request models
-    "StatementParameter",
-    "ExecuteStatementRequest",
-    "GetStatementRequest",
-    "CancelStatementRequest",
-    "CloseStatementRequest",
     "CreateSessionRequest",
     "DeleteSessionRequest",
     # Response models
-    "ExecuteStatementResponse",
-    "GetStatementResponse",
     "CreateSessionResponse",
 ]
