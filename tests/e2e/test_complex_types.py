ad2bb62ed17ca33493c38cdd72686baaa66a12d7
Jothi Prakash <jothi.prakash@databricks.com>
2025-06-16 11:57:10 +0530

    [PECOBLR-330] Support for complex params (#559)
    * Basic testing
    
    * testing examples
    
    * Basic working prototype
    
    * ttypes fix
    
    * Refractored the ttypes
    
    * nit
    
    * Added inline support
    
    * Reordered boolean to be above int
    
    * Check
    
    Working e2e tests prototype
    
    * More tests
    
    * Added unit tests
    
    * refractor
    
    * nit
    
    * nit
    
    * nit
    
    * nit

--- tests/e2e/test_complex_types.py
+++ tests/e2e/test_complex_types.py
@@ -1,5 +1,6 @@
 import pytest
 from numpy import ndarray
+from typing import Sequence
 
 from tests.e2e.test_driver import PySQLPytestTestCase
 
@@ -14,50 +15,73 @@ class TestComplexTypes(PySQLPytestTestCase):
             # Create the table
             cursor.execute(
                 """
-                CREATE TABLE IF NOT EXISTS pysql_e2e_test_complex_types_table (
+                CREATE TABLE IF NOT EXISTS pysql_test_complex_types_table (
                     array_col ARRAY<STRING>,
                     map_col MAP<STRING, INTEGER>,
-                    struct_col STRUCT<field1: STRING, field2: INTEGER>
-                )
+                    struct_col STRUCT<field1: STRING, field2: INTEGER>,
+                    array_array_col ARRAY<ARRAY<STRING>>,
+                    array_map_col ARRAY<MAP<STRING, INTEGER>>,
+                    map_array_col MAP<STRING, ARRAY<STRING>>
+                ) USING DELTA
                 """
             )
             # Insert a record
             cursor.execute(
                 """
-                INSERT INTO pysql_e2e_test_complex_types_table
+                INSERT INTO pysql_test_complex_types_table
                 VALUES (
                     ARRAY('a', 'b', 'c'),
                     MAP('a', 1, 'b', 2, 'c', 3),
-                    NAMED_STRUCT('field1', 'a', 'field2', 1)
+                    NAMED_STRUCT('field1', 'a', 'field2', 1),
+                    ARRAY(ARRAY('a','b','c')),
+                    ARRAY(MAP('a', 1, 'b', 2, 'c', 3)),
+                    MAP('a', ARRAY('a', 'b', 'c'), 'b', ARRAY('d', 'e'))
                 )
                 """
             )
             yield
             # Clean up the table after the test
-            cursor.execute("DROP TABLE IF EXISTS pysql_e2e_test_complex_types_table")
+            cursor.execute("DELETE FROM pysql_test_complex_types_table")
 
     @pytest.mark.parametrize(
         "field,expected_type",
-        [("array_col", ndarray), ("map_col", list), ("struct_col", dict)],
+        [
+            ("array_col", ndarray),
+            ("map_col", list),
+            ("struct_col", dict),
+            ("array_array_col", ndarray),
+            ("array_map_col", ndarray),
+            ("map_array_col", list),
+        ],
     )
     def test_read_complex_types_as_arrow(self, field, expected_type, table_fixture):
         """Confirms the return types of a complex type field when reading as arrow"""
 
         with self.cursor() as cursor:
             result = cursor.execute(
-                "SELECT * FROM pysql_e2e_test_complex_types_table LIMIT 1"
+                "SELECT * FROM pysql_test_complex_types_table LIMIT 1"
             ).fetchone()
 
         assert isinstance(result[field], expected_type)
 
-    @pytest.mark.parametrize("field", [("array_col"), ("map_col"), ("struct_col")])
+    @pytest.mark.parametrize(
+        "field",
+        [
+            ("array_col"),
+            ("map_col"),
+            ("struct_col"),
+            ("array_array_col"),
+            ("array_map_col"),
+            ("map_array_col"),
+        ],
+    )
     def test_read_complex_types_as_string(self, field, table_fixture):
         """Confirms the return type of a complex type that is returned as a string"""
         with self.cursor(
             extra_params={"_use_arrow_native_complex_types": False}
         ) as cursor:
             result = cursor.execute(
-                "SELECT * FROM pysql_e2e_test_complex_types_table LIMIT 1"
+                "SELECT * FROM pysql_test_complex_types_table LIMIT 1"
             ).fetchone()
 
         assert isinstance(result[field], str)
