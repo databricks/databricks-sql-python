39a053060949ef8c60228b073553efd014e850e7
Sai Shree Pradhan <saishree.pradhan@databricks.com>
2025-06-19 15:29:50 +0530

    called connection.close() in test_arraysize_buffer_size_passthrough
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -264,6 +264,7 @@ class ClientTestSuite(unittest.TestCase):
 
         self.assertEqual(kwargs["arraysize"], 999)
         self.assertEqual(kwargs["result_buffer_size_bytes"], 1234)
+        connection.close()
 
     def test_closing_result_set_with_closed_connection_soft_closes_commands(self):
         mock_connection = Mock()
