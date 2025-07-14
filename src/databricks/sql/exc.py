8517ce8d503d97ca0705eca71b1e55315f6e4d64
Sai Shree Pradhan <saishree.pradhan@databricks.com>
2025-07-14 18:37:53 +0530

    exc changes
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>

--- src/databricks/sql/exc.py
+++ src/databricks/sql/exc.py
@@ -1,10 +1,9 @@
 import json
 import logging
 
-from databricks.sql.telemetry.telemetry_client import TelemetryClientFactory
-
 logger = logging.getLogger(__name__)
 from databricks.sql.telemetry.telemetry_client import TelemetryClientFactory
+
 
 ### PEP-249 Mandated ###
 # https://peps.python.org/pep-0249/#exceptions
