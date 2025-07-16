28bf612a5d6e76c5b3f455676969951c2a76406e
Sai Shree Pradhan <saishree.pradhan@databricks.com>
2025-07-16 14:58:56 +0530

    error log for auth provider, ThriftDatabricksClient
    Signed-off-by: Sai Shree Pradhan <saishree.pradhan@databricks.com>

--- src/databricks/sql/session.py
+++ src/databricks/sql/session.py
@@ -10,6 +10,7 @@ from databricks.sql import USER_AGENT_NAME
 from databricks.sql.backend.thrift_backend import ThriftDatabricksClient
 from databricks.sql.backend.databricks_client import DatabricksClient
 from databricks.sql.backend.types import SessionId
+from databricks.sql.telemetry.telemetry_client import TelemetryClientFactory
 
 logger = logging.getLogger(__name__)
 
@@ -40,9 +41,18 @@ class Session:
         self.catalog = catalog
         self.schema = schema
 
-        self.auth_provider = get_python_sql_connector_auth_provider(
-            server_hostname, **kwargs
-        )
+        try:
+            self.auth_provider = get_python_sql_connector_auth_provider(
+                server_hostname, **kwargs
+            )
+        except Exception as e:
+            TelemetryClientFactory.connection_failure_log(
+                error_name="Exception",
+                error_message=str(e),
+                host_url=server_hostname,
+                http_path=http_path,
+                port=self.port,
+            )
 
         user_agent_entry = kwargs.get("user_agent_entry")
         if user_agent_entry is None:
@@ -74,16 +84,26 @@ class Session:
             tls_client_cert_key_password=kwargs.get("_tls_client_cert_key_password"),
         )
 
-        self.backend: DatabricksClient = ThriftDatabricksClient(
-            self.host,
-            self.port,
-            http_path,
-            (http_headers or []) + base_headers,
-            self.auth_provider,
-            ssl_options=self._ssl_options,
-            _use_arrow_native_complex_types=_use_arrow_native_complex_types,
-            **kwargs,
-        )
+        try:
+            self.backend: DatabricksClient = ThriftDatabricksClient(
+                self.host,
+                self.port,
+                http_path,
+                (http_headers or []) + base_headers,
+                self.auth_provider,
+                ssl_options=self._ssl_options,
+                _use_arrow_native_complex_types=_use_arrow_native_complex_types,
+                **kwargs,
+            )
+        except Exception as e:
+            TelemetryClientFactory.connection_failure_log(
+                error_name="Exception",
+                error_message=str(e),
+                host_url=server_hostname,
+                http_path=http_path,
+                port=self.port,
+                user_agent=self.useragent_header,
+            )
 
         self.protocol_version = None
 
