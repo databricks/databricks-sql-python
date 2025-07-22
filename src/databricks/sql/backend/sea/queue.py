ad6b356a9b1080b78d19f80471dfd532c33abf1c
varun-edachali-dbx <varun.edachali@databricks.com>
2025-07-22 07:27:55 +0530

    Revert "acquire lock before notif + formatting (black)"
    This reverts commit ef5836b2ced938ff2426d7971992e2809f8ac42c.

--- src/databricks/sql/backend/sea/queue.py
+++ src/databricks/sql/backend/sea/queue.py
@@ -278,8 +278,7 @@ class LinkFetcher:
             if not links_downloaded:
                 self._shutdown_event.set()
         logger.debug("LinkFetcher[%s]: worker thread exiting", self._statement_id)
-        with self._link_data_update:
-            self._link_data_update.notify_all()
+        self._link_data_update.notify_all()
 
     def start(self):
         """Spawn the worker thread."""
