4fe59197585bba3729f320ebfcf7a2fd31471ea3
varun-edachali-dbx <varun.edachali@databricks.com>
2025-07-22 09:40:12 +0530

    remove redundant ColumnInfo model
    Signed-off-by: varun-edachali-dbx <varun.edachali@databricks.com>

--- src/databricks/sql/backend/sea/models/__init__.py
+++ src/databricks/sql/backend/sea/models/__init__.py
@@ -9,7 +9,6 @@ from databricks.sql.backend.sea.models.base import (
     StatementStatus,
     ExternalLink,
     ResultData,
-    ColumnInfo,
     ResultManifest,
 )
 
@@ -35,7 +34,6 @@ __all__ = [
     "StatementStatus",
     "ExternalLink",
     "ResultData",
-    "ColumnInfo",
     "ResultManifest",
     # Request models
     "StatementParameter",
