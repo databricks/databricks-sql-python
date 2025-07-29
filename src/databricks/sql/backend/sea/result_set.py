6481851917974a54c798f23ab141d61bcb9cc4f0
varun-edachali-dbx <varun.edachali@databricks.com>
2025-07-29 19:23:15 +0530

    more verbose logging for type conversion fail
    Signed-off-by: varun-edachali-dbx <varun.edachali@databricks.com>

--- src/databricks/sql/backend/sea/result_set.py
+++ src/databricks/sql/backend/sea/result_set.py
@@ -92,6 +92,7 @@ class SeaResultSet(ResultSet):
         converted_row = []
 
         for i, value in enumerate(row):
+            column_name = self.description[i][0]
             column_type = self.description[i][1]
             precision = self.description[i][4]
             scale = self.description[i][5]
@@ -103,7 +104,7 @@ class SeaResultSet(ResultSet):
                 converted_row.append(converted_value)
             except Exception as e:
                 logger.warning(
-                    f"Error converting value '{value}' to {column_type}: {e}"
+                    f"Error converting value '{value}' to {column_type} in column {column_name}: {e}"
                 )
                 converted_row.append(value)
 
