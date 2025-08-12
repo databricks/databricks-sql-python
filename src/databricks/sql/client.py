63e5c85ad20fca9a2412a767f229340b2b48548e
Sreekanth Vadigi <sreekanth.vadigi@databricks.com>
2025-08-12 18:41:15 +0530

    linting fix
    Signed-off-by: Sreekanth Vadigi <sreekanth.vadigi@databricks.com>

--- src/databricks/sql/client.py
+++ src/databricks/sql/client.py
@@ -773,7 +773,9 @@ class Cursor:
             headers=headers,
             timeout=300,  # 5 minute timeout
         ) as response:
-            self._validate_staging_http_response(response, UploadType.STREAM_UPLOAD.value)
+            self._validate_staging_http_response(
+                response, UploadType.STREAM_UPLOAD.value
+            )
 
     @log_latency(StatementType.SQL)
     def _handle_staging_put(
